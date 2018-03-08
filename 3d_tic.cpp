80ba2dd169321b7a111a346f85d383687efbb52b
prestaylor51 <prestaylor51@gmail.com>
2018-03-08 11:47:11 -0700

    added BOARD_SIZE #define

--- 3d_tic.cpp
+++ 3d_tic.cpp
@@ -10,17 +10,19 @@
 #include <cassert>
 using namespace std;
 
-bool read(         char board[][3][3], const char* fileName);// MOD
-bool write(  const char board[][3][3], const char* fileName);// MOD
-void display(const char board[][3][3]);// MOD
-bool didWin( const char board[][3][3], char turn);// MOD
+#define BOARD_SIZE 3
+
+bool read(         char board[][BOARD_SIZE][BOARD_SIZE], const char* fileName);// MOD
+bool write(  const char board[][BOARD_SIZE][BOARD_SIZE], const char* fileName);// MOD
+void display(const char board[][BOARD_SIZE][BOARD_SIZE]);// MOD
+bool didWin( const char board[][BOARD_SIZE][BOARD_SIZE], char turn);// MOD
 
 /**********************************************************************
  * Keeps the data and calles the read/display/write functions
  ***********************************************************************/
 int main()
 {
-   char board[3][3][3];// MOD
+   char board[BOARD_SIZE][BOARD_SIZE][BOARD_SIZE];// MOD
 
    // read the board
    char fileName[256];
@@ -51,7 +53,7 @@ int main()
  * READ
  * Read the board from the specified filename
  *************************************************************/
-bool read(char board[][3][3], const char* fileName)// MOD
+bool read(char board[][BOARD_SIZE][BOARD_SIZE], const char* fileName)// MOD
 {
    assert(*fileName);
 
@@ -61,9 +63,9 @@ bool read(char board[][3][3], const char* fileName)// MOD
       return false;
 
    // read 9 symbols, hopefully they are . X O
-   for (int l = 0; l < 3; l++) // NEW
-      for (int r = 0; r < 3; r++)
-            for (int c = 0; c < 3; c++)
+   for (int l = 0; l < BOARD_SIZE; l++) // NEW
+      for (int r = 0; r < BOARD_SIZE; r++)
+            for (int c = 0; c < BOARD_SIZE; c++)
             {
             fin >> board[l][r][c]; // MOD
             assert(!fin.fail());
@@ -81,7 +83,7 @@ bool read(char board[][3][3], const char* fileName)// MOD
  * WRITE
  * Write to fileName the board data
  *********************************************************/
-bool write(const char board[][3][3], const char* fileName)// MOD
+bool write(const char board[][BOARD_SIZE][BOARD_SIZE], const char* fileName)// MOD
 {
    assert(fileName[0] != '\0');
 
@@ -91,9 +93,9 @@ bool write(const char board[][3][3], const char* fileName)// MOD
       return false;
 
    // write my 9 symbols
-   for (int l = 0; l < 3; l++) // NEW
-      for (int r = 0; r < 3; r++)
-            for (int c = 0; c < 3; c++)  
+   for (int l = 0; l < BOARD_SIZE; l++) // NEW
+      for (int r = 0; r < BOARD_SIZE; r++)
+            for (int c = 0; c < BOARD_SIZE; c++)  
                   fout << board[r][c][l] << (c == 2 ? '\n' : ' ');// MOD
 
    // close it!
@@ -106,19 +108,19 @@ bool write(const char board[][3][3], const char* fileName)// MOD
  * DISPLAY
  * Display the contents the the screen
  *****************************************************/
-void display(const char board[][3][3])// MOD
+void display(const char board[][BOARD_SIZE][BOARD_SIZE])// MOD
 {
    // loop through each row
-   for (int l = 0; l < 3; l++) // NEW
+   for (int l = 0; l < BOARD_SIZE; l++) // NEW
    {
-      for (int r = 0; r < 3; r++)
+      for (int r = 0; r < BOARD_SIZE; r++)
       {
             // only the first row is not preceeded with the --+-- magic
             if (r != 0)
             cout << "---+---+---\n";
 
             // now, on each row, do the column stuff
-            for (int c = 0; c < 3; c++)
+            for (int c = 0; c < BOARD_SIZE; c++)
             {
             // display a space for the dot
             if (board[r][c][l] == '.') // MOD
@@ -147,7 +149,7 @@ void display(const char board[][3][3])// MOD
  * Did a given player (determined by the "turn"
  * variable) win the game?
  *******************************************/
-bool didWin(const char board[][3][3], char turn)// MOD
+bool didWin(const char board[][BOARD_SIZE][BOARD_SIZE], char turn)// MOD
 {
    // vertical
 
